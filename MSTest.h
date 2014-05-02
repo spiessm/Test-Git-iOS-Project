a554cc55feefffc0799db1dd8319ac78667b4d76
Martin Spiess <martin@bioz5-msimac.fritz.box>
2014-05-02 22:02:16 +0200

    added test class

---
+++ MSTest.h
@@ -0,0 +1,13 @@
+//
+//  MSTest.h
+//  Git Test
+//
+//  Created by Martin Spiess on 2.5.14.
+//  Copyright (c) 2014 Molecular Samples. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSTest : NSObject
+
+@end
---
+++ MSTest.m
@@ -0,0 +1,13 @@
+//
+//  MSTest.m
+//  Git Test
+//
+//  Created by Martin Spiess on 2.5.14.
+//  Copyright (c) 2014 Molecular Samples. All rights reserved.
+//
+
+#import "MSTest.h"
+
+@implementation MSTest
+
+@end
